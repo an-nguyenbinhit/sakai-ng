0edfa537514415ad0056fd2362dacf4857184bf4
an.nguyenbinh <nguyenan6197@gmail.com>
2026-03-04 10:09:42 +0700

    chore: remove unused encode-decode-menu component

--- src/app/pages/encode-decode-menu/encode-decode-menu.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-encode-decode-menu',
-    imports: [],
-    templateUrl: './encode-decode-menu.html',
-    styleUrl: './encode-decode-menu.scss'
-})
-export class EncodeDecodeMenu {}
