ba1834c1d9547f179790be95d7a71faaaa46d937
an.nguyenbinh <nguyenan6197@gmail.com>
2026-03-05 13:59:41 +0700

    feat: Add Angular Service Worker for Progressive Web App (PWA) support and configure Vercel caching headers.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,10 +1,12 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { provideMonacoEditor } from 'ngx-monaco-editor-v2';
 import { appRoutes } from './app.routes';
+import { isDevMode } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -12,6 +14,10 @@ export const appConfig: ApplicationConfig = {
         provideHttpClient(withFetch()),
         provideZonelessChangeDetection(),
         providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
-        provideMonacoEditor()
+        provideMonacoEditor(),
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        })
     ]
 };
